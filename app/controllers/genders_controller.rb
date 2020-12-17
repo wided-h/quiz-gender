d2043e0e890d07567525a152ed4467b8523f9301
wided-h <benhattabwided@hotmail.fr>
2020-12-17 18:21:39 +0100

    page 2

--- app/controllers/genders_controller.rb
+++ app/controllers/genders_controller.rb
@@ -5,11 +5,11 @@ class GendersController < ActionController::Base
   end
 
   def guess_gender
-    render '/genders/guess_gender'
-    file = File.open('./mf.txt')
+    file=File.open(File.dirname(mf.txt__FILE__) + './mf.txt')
     lines = file.readlines
     matchdata = lines.map { |line| line.chop.match(/(.*)\..*:(.+)/) }
     words = matchdata.map { |matchdata| [matchdata[1], matchdata[2]] }.to_h
+    puts words
   end
 
   def final_score
