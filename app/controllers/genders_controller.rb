473d8a7ea585cc9a0d02741a6283d5ce01fc2ac4
wided-h <benhattabwided@hotmail.fr>
2020-12-17 17:25:56 +0100

    deuxième partie: du site_gender affichage des mots aléatoirement sur la page 2

--- app/controllers/genders_controller.rb
+++ app/controllers/genders_controller.rb
@@ -6,6 +6,10 @@ class GendersController < ActionController::Base
 
   def guess_gender
     render '/genders/guess_gender'
+    file = File.open('./mf.txt')
+    lines = file.readlines
+    matchdata = lines.map { |line| line.chop.match(/(.*)\..*:(.+)/) }
+    words = matchdata.map { |matchdata| [matchdata[1], matchdata[2]] }.to_h
   end
 
   def final_score
