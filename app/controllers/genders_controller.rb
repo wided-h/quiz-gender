a25ea88a52359a2591161f3348859211e1416b8d
wided-h <benhattabwided@hotmail.fr>
2020-12-18 17:48:47 +0100

    page 2:lire le dictionnaire+ lignes lues+ mise à disposition des données du dict

--- app/controllers/genders_controller.rb
+++ app/controllers/genders_controller.rb
@@ -5,18 +5,12 @@ class GendersController < ActionController::Base
   end
 
   def guess_gender
-    render '/genders/guess_gender'
     file = File.open('./mf.txt')
     lines = file.readlines
     matchdata = lines.map { |line| line.chop.match(/(.*)\..*:(.+)/) }
-    words = matchdata.map { |matchdata| [matchdata[1], matchdata[2]] }.to_h
-    count_word = 0
-    words_limit = 20
-    while count_word < words_limit do
-      count_word += 1
-      word = words.keys.sample
-      puts "#{word}"
-    end
+    @words = matchdata.map { |matchdata| [matchdata[1], matchdata[2]] }.to_h
+    @word = @words.keys.sample
+    render '/genders/guess_gender'
     end
 
   def final_score
