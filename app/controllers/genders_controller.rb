b2c2b59a10dcdeef2349c7beb106843a58886f9d
wided-h <benhattabwided@hotmail.fr>
2021-08-17 10:32:11 +0100

    fix comma bug

--- app/controllers/genders_controller.rb
+++ app/controllers/genders_controller.rb
@@ -9,7 +9,10 @@ class GendersController < ApplicationController
     file = File.open('./mf.txt')
     lines = file.readlines
     matchdata = lines.map { |line| line.chop.match(/(.*)\..*:(.+)/) }
-    words = matchdata.map { |matchdata| [matchdata[1], matchdata[2]] }
+    words = matchdata.map { |matchdata|
+      word=matchdata[1].split(',')[0] #matchdata[1]=="string1,string2"
+      [word, matchdata[2]]
+    }
     @words=words.sample(n).to_h
     render '/genders/guess_gender'
     end
